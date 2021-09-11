a299c37672b33a689e7cb93f521294d383ddc1f2
sabrimacia <samacia87@gmail.com>
2021-09-11 14:33:57 -0300

    tarea dos

---
+++ main.js
@@ -0,0 +1,28 @@
+/* EJERCICO CLASE 1
+let nombre = prompt("Ingresa tu nombre");
+let edadIngresada = prompt("Ingresa tu edad");
+let edad = parseFloat(edadIngresada);
+console.log("Bienvenido" + nombre );
+let fecha_nacimiento = 1992;
+let edad_actual = 29;
+let suma = fecha_nacimiento + edad_actual;
+alert("Hola" +  " " + nombre + " " + "estamos en el año" + " " + suma);
+*/
+
+/*EJERCICIO CLASE 2 */ 
+let numero=prompt("Ingrese un numero por favor");
+if(numero >1000){
+    alert("El numero es mayor");
+}
+
+let texto=prompt("Ingrese un texto por favor");
+if((texto == "hola") || (texto == "HOLA")){
+    alert("Has dicho hola");
+}
+
+let numeroIngresado=prompt("Ingrese cualquier numero");
+if((numeroIngresado >=10) && (numeroIngresado <=50)){
+    alert("El numero ingresado esta entre 10 y 50");
+}else{
+    alert("Error");
+}
